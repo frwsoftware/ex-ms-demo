41e83bbb5b8e2458de73c4e875f98011e2354329
frwsoftware <frwsoftware@gmail.com>
2023-05-11 21:43:22 +0300

    init spring boot

---
+++ src/main/java/com/frwsoftware/exmsdemo/ExMsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.frwsoftware.exmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExMsDemoApplication.class, args);
+	}
+
+}
